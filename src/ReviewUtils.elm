d86db9d434d070d9e745e6a09991f5a6386f1190
Leo <leandro.martin@protonmail.com>
2025-03-05 04:00:41 +0000

    Refactor ReviewUtils.elm to remove unnecessary dark mode classes and improve styling consistency

--- src/ReviewUtils.elm
+++ src/ReviewUtils.elm
@@ -280,14 +280,14 @@ showReview translation review =
     Route.link
         [ Attrs.class "cursor-pointer" ]
         [ Html.div
-            [ Attrs.class "block max-w-sm p-6 bg-white rounded-lg hover:bg-gray-100 relative" ]
+            [ Attrs.class "block max-w-sm p-6 rounded-lg hover:bg-gray-100 relative" ]
             [ Html.div
                 [ Attrs.class "flex items-center mb-2 space-x-4" ]
                 [ Html.span
                     [ Attrs.class "px-2 py-1 text-xs font-semibold text-white bg-gray-400 rounded" ]
                     [ Html.text <| userRoleEnumToTranslatedString translation review.role ]
                 , Html.span
-                    [ Attrs.class "mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white" ]
+                    [ Attrs.class "mb-2 text-2xl font-bold tracking-tight text-gray-900" ]
                     [ Html.text review.name ]
                 , case review.rating of
                     Just rating ->
@@ -301,11 +301,11 @@ showReview translation review =
                 ]
             , if currentLanguage == Maybe.withDefault "en" review.language then
                 Html.div
-                    [ Attrs.class "text-gray-700 dark:text-gray-400" ]
+                    [ Attrs.class "text-gray-700" ]
                     [ case review.content of
                         Just comment ->
                             Html.p
-                                [ Attrs.class "font-normal text-gray-700 dark:text-gray-400 " ]
+                                [ Attrs.class "font-normal text-gray-700" ]
                                 [ Html.text (trimComment comment) ]
 
                         Nothing ->
@@ -317,17 +317,17 @@ showReview translation review =
 
               else
                 Html.div
-                    [ Attrs.class "text-gray-700 dark:text-gray-400 mb-2" ]
+                    [ Attrs.class "text-gray-700 mb-2" ]
                     [ case review.content of
                         Just _ ->
                             Html.p
-                                [ Attrs.class "font-normal text-gray-700 dark:text-gray-400 " ]
+                                [ Attrs.class "font-normal text-gray-700" ]
                                 [ Html.text <| getTranslation currentLanguage review.translations ]
 
                         Nothing ->
                             Html.text ""
                     , Html.div
-                        [ Attrs.class "text-xs text-gray-500 italic  mt-2" ]
+                        [ Attrs.class "text-xs text-gray-500 italic mt-2" ]
                         [ Html.text <| Maybe.withDefault "" (Maybe.map (\time -> String.left 10 (Iso8601.fromTime time)) review.submittedAt) ]
                     , Html.div
                         [ Attrs.class "absolute bottom-2 right-2 text-xs text-gray-500 italic" ]
@@ -419,7 +419,7 @@ ratingToStars rating =
                     Phosphor.Fill
                     |> withSize 18
                     |> withSizeUnit "px"
-                    |> toHtml [ Attrs.class "text-primary-500" ]
+                    |> toHtml [ Attrs.class "fill-(--color-yellow-500)" ]
                 )
             )
         |> List.map (\x -> x)
